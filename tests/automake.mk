04d1660453aec92fb1f6e1fa396afda224e24c7a
Erlon R. Cruz <erlon@canonical.com>
2025-07-24 16:20:28 -0300

    northd: ACL correctly handles UDP fragmented traffic.
    At the current state, OVN can not handle UDP fragmented traffic
    for ACLs in the userspace. Just like in the case of LB (commit
    20a96b9), the kernel DP will try to reassemble the fragments during CT
    lookup, however userspace won't reassemble them. One way to solve that
    is to use the ct_udp field to match on ct.new connections. With the
    current state, OVN will get the CMS rules and write them verbatim
    to SB, which will generate similar rules in OVS. So, this workaround
    replaces L4 protocol matches with connection tracking  equivalents.
    For example:
    
       "outport == "server" && udp && udp.dst == 4242"
    becomes:
       "outport == "server" && ct.new && udp && ct_udp.dst == 4242"
    
    We made this behavior to be optional and disabled by default. In
    order to enable it, the user needs to set an NB_Global flag:
    
    ovn-nbctl set NB_Global . options:acl_udp_ct_translation=true
    
    Signed-off-by: Erlon R. Cruz <erlon@canonical.com>

--- tests/automake.mk
+++ tests/automake.mk
@@ -272,7 +272,9 @@ CHECK_PYFILES = \
 	tests/uuidfilt.py \
 	tests/test-tcp-rst.py \
 	tests/check_acl_log.py \
-	tests/scapy-server.py
+	tests/scapy-server.py \
+	tests/udp_client.py \
+	tests/tcp_simple.py
 
 EXTRA_DIST += $(CHECK_PYFILES)
 PYCOV_CLEAN_FILES += $(CHECK_PYFILES:.py=.py,cover) .coverage
